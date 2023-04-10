fa7c9490fba14ef08cedeef6e4562a5a55b01bda
morita-koki <morita82007koki@gmail.com>
2023-04-11 03:07:47 +0900

    about page作成

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,38 +8,62 @@ import Image  from "next/image"
 export default function About() {
   return (
     <Container maxWidth="md">
+        <Typography variant="h3" textAlign="center" marginY={3}>
+            About Me
+        </Typography>
         <Grid
             container
-            direction="row"
-            justifyContent="space-evenly"
+            // direction="row"
+            justifyContent="center"
             alignItems="center" >
-            <Grid item xs={12}>
-                <Typography variant="h3" textAlign="center">
-                    About Me
-                </Typography>
-                <Grid container>
-                  <Grid item xs={12} md={4}>
-                    <Image
-                      src="/penguin.jpeg"
-                      alt="jack's icon"
-                      loading="lazy"
-                      width={100}
-                      height={100}
-                    />
-                  </Grid>
-                  <Grid item xs={12} md={8}>
-                    <Typography variant="caption">
-                      大阪府出身の大学４年生。大学で電気工学、電子工学を学んでいて、大学院では情報学を専攻する予定。
-                      学部２年生のときにアプリ開発団体jackに所属し、サークルの仲間と一緒にアプリ開発、WEBサイト制作、ハッカソンなどしています。
-                      バックエンド、フロントエンドともに興味の赴くままに触れる。
-                    </Typography>
-                  </Grid>
-                </Grid>
-                <Box component="ul" sx={{listStyle: "none", paddingLeft: "20%"}}>
-                  <li>GitHub: <Box component="a" href="https://github.com/morita-koki" target="_blank" sx={{textDecoration: "underline", color: "#0000EE"}}>https://github.com/morita-koki</Box></li>
-                  <li>アプリ開発団体jack: <Box component="a" href="https://twitter.com/jackapp_tmy" target="_blank" sx={{textDecoration: "underline", color: "#0000EE"}}>https://twitter.com/jackapp_tmy</Box></li>
-                </Box> 
+
+            <Grid container justifyContent="center" alignItems="center" item xs={12} md={5} marginBottom={2}>
+              <Grid container alignItems="center" item xs={6} md={5}>
+                <Image
+                  src="/penguin.jpeg"
+                  alt="koki's icon"
+                  loading="lazy"
+                  width={100}
+                  height={100}
+                  style={{ margin: "auto" }}
+                />
+              </Grid>
+              <Grid item xs={8} md={5}>
+                <Typography textAlign="center" component="p" variant="caption">大阪出身</Typography>
+                <Typography textAlign="center" component="p" variant="caption">2001年5月17日生</Typography>
+                <Typography textAlign="center" component="p" variant="caption">名古屋大学所属</Typography>
+              </Grid>
             </Grid>
+
+            <Grid item xs={10} md={10} marginBottom={2}>
+              <Typography variant="subtitle1">
+                大阪出身の大学４年生。大学で電気工学、電子工学を学んでいて、大学院では情報学を専攻する予定。
+                学部２年生のときにアプリ開発団体jackに出会いアプリ開発の楽しさに魅入られる。
+                サークルの仲間と一緒にアプリ開発、WEBサイト制作、ハッカソンなど
+                バックエンド、フロントエンドともに興味の赴くままに触れる。
+              </Typography>
+            </Grid>
+
+            {/* <Grid container alignItems="center" item xs={12} md={6}>
+              <Image
+                src="/jack-icon.jpg"
+                alt="jack's icon"
+                loading="lazy"
+                width={100}
+                height={100}
+                style={{ margin: "auto" }}
+              />
+            </Grid> */}
+
+            <Grid item xs={8} md={4}>
+              <Typography component="p" variant="subtitle2">
+                - GitHub: <Box component="a" href="https://github.com/morita-koki" target="_blank" sx={{textDecoration: "none", color: "#039be5"}}>https://github.com/morita-koki</Box>
+              </Typography>
+              <Typography component="p" variant="subtitle2">
+                - jack: <Box component="a" href="https://twitter.com/jackapp_tmy" target="_blank" sx={{textDecoration: "none", color: "#039be5"}}>https://twitter.com/jackapp_tmy</Box>
+              </Typography>
+            </Grid>
+            
         </Grid>
     </Container>
   )
